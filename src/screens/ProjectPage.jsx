04734be46d49caaf61a9ca9ae4ae16e68c7e151f
Alex Vo <alexanvo@tamu.edu>
2024-06-30 20:00:24 -0500

    more projecti info added

--- src/screens/ProjectPage.jsx
+++ src/screens/ProjectPage.jsx
@@ -9,17 +9,43 @@ const ProjectPage = () => {
   const { projectId } = useParams();
   const projectDetails = {
     split: {
-      type: "Web Development",
-      title: "Sp/it (Roommate App)",
+      type: "App Development",
+      title: "Sp/it",
+      subtitle: "Roommate App",
       desc: "Sp/lit is a quality-of-life mobile application that combines multiple cooperative \
       features into one place to help roommates have a better time staying with each other. \
       Useful features offered to users are messenging, shared calendars, finance trackers, location sharing, and multi-task management.",
-      media: ['/images/capsy-product-page.png', '/images/split-product-page.png'],
+      media: ['/images/split-product-page.png'],
       link: "https://capsy.vercel.app/",
       tech: "React, Firebase, PostgreSQL, Express, JavaScript, NodeJS",
       nextProjectId: "tabventure",
       prevProjectId: "capsy",
     },
+    tabventure: {
+      type: "Web Development",
+      title: "Tabventure",
+      subtitle: "AI-Powered Trip Planner",
+      desc: "Tabventure is a browser extension application that has the ability to summarize the content of the currently open tabs. \
+      This is incredibly useful for someone who tends to open many tabs to look at multiple outlets of information regarding their trip. \
+      When activated, the extension displays a window housing a virtualized AI assistant specifically designed to answer questions based \
+      on the open browser tabs. This allows for online trip planning to become streamlined and alleviates the hassle of dealing with multiple open tabs yourself.",
+      media: ['/images/tabventure-product-page.png'],
+      link: "",
+      tech: "React, Firebase, Python, ChromaDB, OpenAI, LangChain, JavaScript, NodeJS",
+      nextProjectId: "capsy",
+      prevProjectId: "split",
+    },
+    capsy: {
+      type: "Web Development",
+      title: "Capsy",
+      subtitle: "Virtual Time Capsule",
+      desc: "",
+      media: ['/images/capsy-product-page.png'],
+      link: "https://capsy.vercel.app",
+      tech: "React, Firebase, JavaScript, Express, NodeJS, PostgreSQL",
+      nextProjectId: "split",
+      prevProjectId: "tabventure",
+    }
   };
 
   const project = projectDetails[projectId];
